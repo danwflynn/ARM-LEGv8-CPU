7a8f445257e4b2830d74ab4466a4d642b23acbda
danwflynn <flynn.da@northeastern.edu>
2025-12-12 16:13:47 -0500

    get leafs of keyword

--- schematic_generator.py
+++ schematic_generator.py
@@ -77,13 +77,33 @@ def strip_verilog(lines):
           result.append(part)
   return result
 
+
 def get_subset(module_name, lines):
   result = []
   inside_module = False
   for line in lines:
-    if "module " in line and module_name in line: inside_module = True
+    if "module " in line and (module_name + " " in line or module_name + "(" in line): inside_module = True
     if inside_module: result.append(line)
     if "endmodule" in line and inside_module: return result
+  raise ValueError(f"{module_name} module not found or not correctly instantiated.\nmodule {module_name}( must be on 1 line.")
+
+
+def get_leafs_of_keyword(subset, keyword):
+  input_names = []
+  input = ""
+  for line in subset:
+    if line.startswith(keyword):
+      outside_brackets = True
+      for char in line[len(keyword):]:
+        if char == " ": continue
+        elif char == "[": outside_brackets = False
+        elif char == "]": outside_brackets = True
+        elif outside_brackets and char != "," and char != ";": input += char
+        elif outside_brackets and (char == "," or char == ";"):
+          input_names.append(input)
+          input = ""
+  return input_names
+
 
 def generate_schematic(module_name):
   schematic = Schematic(module_name)
@@ -105,8 +125,8 @@ def generate_schematic(module_name):
       print(f"Error: listed file '{vfile}' does not exist.")
       sys.exit(1)
   
-  for line in get_subset(module_name, all_lines):
-    print(line)
+  for i in get_leafs_of_keyword(get_subset(module_name, all_lines), "input"): print(i)
+  for i in get_leafs_of_keyword(get_subset(module_name, all_lines), "inout"): print(i)
 
 
 if __name__ == '__main__':
